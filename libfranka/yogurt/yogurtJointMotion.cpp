8524d2ef124050cc5ca892f62a731d87e5b4009f
Haopeng <philthinker@sina.com>
2020-05-24 15:08:04 +0800

    Learning Joint Position Control

--- libfranka/yogurt/yogurtJointMotion.cpp
+++ libfranka/yogurt/yogurtJointMotion.cpp
@@ -12,30 +12,39 @@
 
 int main(int argc, char** argv) {
     if (argc != 2) {
-    std::cerr << "Usage: " << argv[0] << " <robot-hostname>" << std::endl;
+    std::cerr << "Usage: " << argv[0] << " 172.16.0.2" << std::endl;
     return -1;
   }
   try
   {
       franka::Robot robot(argv[1]);
-      setDefaultBehavior(robot);
-      std::array<double, 7> q_goal = {{0, -M_PI_4, 0, -3 * M_PI_4, 0, M_PI_2, M_PI_4}};
-      MotionGenerator motion_generator(0.5, q_goal);
-      std::cout << "WARNING: This example will move the robot! "
+      //setDefaultBehavior(robot);
+      robot.setCollisionBehavior(
+      {{20.0, 20.0, 20.0, 20.0, 20.0, 20.0, 20.0}}, {{20.0, 20.0, 20.0, 20.0, 20.0, 20.0, 20.0}},
+      {{10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0}}, {{10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0}},
+      {{20.0, 20.0, 20.0, 20.0, 20.0, 20.0}}, {{20.0, 20.0, 20.0, 20.0, 20.0, 20.0}},
+      {{10.0, 10.0, 10.0, 10.0, 10.0, 10.0}}, {{10.0, 10.0, 10.0, 10.0, 10.0, 10.0}});
+      robot.setJointImpedance({{3000, 3000, 3000, 2500, 2500, 2000, 2000}});
+      robot.setCartesianImpedance({{3000, 3000, 3000, 300, 300, 300}});
+      //std::array<double, 7> q_goal = {{0, -M_PI_4, 0, -3 * M_PI_4, 0, M_PI_2, M_PI_4}};
+      //MotionGenerator motion_generator(0.5, q_goal);
+      std::cout << "WARNING: This code will move the robot! "
               << "Please make sure to have the user stop button at hand!" << std::endl
               << "Press Enter to continue..." << std::endl;
       std::cin.ignore();
-      robot.control(motion_generator);
-      std::cout << "Finished moving to initial joint configuration." << std::endl;
+      //robot.control(motion_generator);
+      //std::cout << "Finished moving to initial joint configuration." << std::endl;
 
       // Set additional parameters always before the control loop, NEVER in the control loop!
       // Set collision behavior.
+      /*
       robot.setCollisionBehavior(
         {{20.0, 20.0, 18.0, 18.0, 16.0, 14.0, 12.0}}, {{20.0, 20.0, 18.0, 18.0, 16.0, 14.0, 12.0}},
         {{20.0, 20.0, 18.0, 18.0, 16.0, 14.0, 12.0}}, {{20.0, 20.0, 18.0, 18.0, 16.0, 14.0, 12.0}},
         {{20.0, 20.0, 20.0, 25.0, 25.0, 25.0}}, {{20.0, 20.0, 20.0, 25.0, 25.0, 25.0}},
         {{20.0, 20.0, 20.0, 25.0, 25.0, 25.0}}, {{20.0, 20.0, 20.0, 25.0, 25.0, 25.0}});
-
+      */
+      
       std::array<double, 7> initial_position;
       double time = 0.0;
       robot.control([&initial_position, &time](const franka::RobotState& robot_state,
@@ -48,16 +57,17 @@ int main(int argc, char** argv) {
       double delta_angle = M_PI / 8.0 * (1 - std::cos(M_PI / 2.5 * time));
 
       franka::JointPositions output = {{initial_position[0], initial_position[1],
-                                        initial_position[2], initial_position[3] + delta_angle,
-                                        initial_position[4] + delta_angle, initial_position[5],
+                                        initial_position[2], initial_position[3],
+                                        initial_position[4], initial_position[5],
                                         initial_position[6] + delta_angle}};
-
-      if (time >= 5.0) {
-        std::cout << std::endl << "Finished motion, shutting down example" << std::endl;
+      //std::cout << time << std::endl;
+      if (time >= 5) {
+        std::cout << std::endl << "Finished motion, shutting down yogurt" << std::endl;
         return franka::MotionFinished(output);
       }
       return output;
     });
+    
   }
   catch(const franka::Exception& e)
   {
