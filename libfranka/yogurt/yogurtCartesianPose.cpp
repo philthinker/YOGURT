f22d76ed8c25d4d39767b2c6ad4e03034a6d5e6b
Haopeng <philthinker@sina.com>
2020-05-25 14:56:49 +0800

    Learn to design controller

--- libfranka/yogurt/yogurtCartesianPose.cpp
+++ libfranka/yogurt/yogurtCartesianPose.cpp
@@ -1,46 +1,28 @@
-// Copyright (c) 2017 Franka Emika GmbH
-// Use of this source code is governed by the Apache-2.0 license, see LICENSE
 #include <cmath>
 #include <iostream>
 
 #include <franka/exception.h>
 #include <franka/robot.h>
 
-#include "examples_common.h"
-
-/**
- * @example generate_cartesian_pose_motion.cpp
- * An example showing how to generate a Cartesian motion.
- *
- * @warning Before executing this example, make sure there is enough space in front of the robot.
- */
-
 int main(int argc, char** argv) {
-  if (argc != 2) {
+    if (argc != 2) {
     std::cerr << "Usage: " << argv[0] << " <robot-hostname>" << std::endl;
     return -1;
   }
   try {
     franka::Robot robot(argv[1]);
-    setDefaultBehavior(robot);
-
-    // First move the robot to a suitable joint configuration
-    std::array<double, 7> q_goal = {{0, -M_PI_4, 0, -3 * M_PI_4, 0, M_PI_2, M_PI_4}};
-    MotionGenerator motion_generator(0.5, q_goal);
-    std::cout << "WARNING: This example will move the robot! "
-              << "Please make sure to have the user stop button at hand!" << std::endl
-              << "Press Enter to continue..." << std::endl;
-    std::cin.ignore();
-    robot.control(motion_generator);
-    std::cout << "Finished moving to initial joint configuration." << std::endl;
-
-    // Set additional parameters always before the control loop, NEVER in the control loop!
-    // Set collision behavior.
     robot.setCollisionBehavior(
         {{20.0, 20.0, 18.0, 18.0, 16.0, 14.0, 12.0}}, {{20.0, 20.0, 18.0, 18.0, 16.0, 14.0, 12.0}},
         {{20.0, 20.0, 18.0, 18.0, 16.0, 14.0, 12.0}}, {{20.0, 20.0, 18.0, 18.0, 16.0, 14.0, 12.0}},
         {{20.0, 20.0, 20.0, 25.0, 25.0, 25.0}}, {{20.0, 20.0, 20.0, 25.0, 25.0, 25.0}},
         {{20.0, 20.0, 20.0, 25.0, 25.0, 25.0}}, {{20.0, 20.0, 20.0, 25.0, 25.0, 25.0}});
+    robot.setJointImpedance({{3000, 3000, 3000, 2500, 2500, 2000, 2000}});
+    robot.setCartesianImpedance({{3000, 3000, 3000, 300, 300, 300}});
+
+    std::cout << "WARNING: This yogurt will move the robot! "
+              << "Please make sure to have the user stop button at hand!" << std::endl
+              << "Press Enter to continue..." << std::endl;
+    std::cin.ignore();
 
     std::array<double, 16> initial_pose;
     double time = 0.0;
--- libfranka/yogurt/yogurtJointVelocity.cpp
+++ libfranka/yogurt/yogurtJointVelocity.cpp
@@ -5,8 +5,6 @@
 #include <franka/exception.h>
 #include <franka/robot.h>
 
-#include "examples_common.h"
-
 
 int main(int argc, char** argv) {
   if (argc != 2) {
@@ -26,15 +24,10 @@ int main(int argc, char** argv) {
     robot.setJointImpedance({{3000, 3000, 3000, 2500, 2500, 2000, 2000}});
     robot.setCartesianImpedance({{3000, 3000, 3000, 300, 300, 300}});
 
-    // First move the robot to a suitable joint configuration
-    //std::array<double, 7> q_goal = {{0, -M_PI_4, 0, -3 * M_PI_4, 0, M_PI_2, M_PI_4}};
-    //MotionGenerator motion_generator(0.5, q_goal);
     std::cout << "WARNING: This yogurt will move the robot! "
               << "Please make sure to have the user stop button at hand!" << std::endl
               << "Press Enter to continue..." << std::endl;
     std::cin.ignore();
-    //robot.control(motion_generator);
-    //std::cout << "Finished moving to initial joint configuration." << std::endl;
 
     double time_max = 1.0;
     double omega_max = 2.0;
