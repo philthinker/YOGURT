21c8eb645969fd970125fa95644187353450bc96
Haopeng <philthinker@sina.com>
2020-06-07 17:06:58 +0800

    Start recording EFT

---
+++ libfranka/yogurt/yogurtGetEFT.cpp
@@ -0,0 +1,7 @@
+//yogurtGetEFT
+// Record the external 6th dim. of force and torque by franka's internal dynamics
+//
+// Haopeng Hu
+// 2020.06.07, HIT's 100th anniversary
+// All rights reserved
+
